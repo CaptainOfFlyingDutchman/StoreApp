124a637ef7fa894ecb2718536c2e61de02fb6376
Manvendra Singh <manvendrask2@gmail.com>
2017-10-09 10:25:13 +0530

    Added footer screen that contains a signature capture button. However it is not working right now. Changed permission of android/gradlew to execute in order to run the project on Linux machine.

--- app/components/steps/Footer.js
+++ app/components/steps/Footer.js
@@ -1,27 +1,60 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity,TouchableHighlight, Image, TextInput } from 'react-native';
+import IconFA from 'react-native-vector-icons/FontAwesome';
+import Sign from '../Sign';
 
 class Footer extends Component {
   static navigationOptions = ({ navigation }) => ({
-    title: 'Footer'
+    title: 'Footer',
+    tabBarIcon: () => <IconFA name="cloud" size={22} />
   });
+  constructor(props) {
+    super(props);
 
+    this.state = {
+        sign: {}
+    };
+    this._openSign = this._openSign.bind(this);
+  }
+
+  _openSign() {
+    this.props.navigation.navigate('Sign');
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Footer</Text>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <TouchableHighlight style={styles.buttonStyle}
+              onPress={() => { this._openSign() } } >
+              <Text>Sign</Text>
+          </TouchableHighlight>
+          </View>
       </View>
     );
   }
-}
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2c3e50',
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'flex-start',
+      alignItems: 'flex-start',
+      flexDirection: 'row'
+    },
+    buttonContainer: {
+      backgroundColor: 'blue',
+      borderRadius: 5,
+    },
+    signature: {
+      flex: 1,
+      borderColor: '#000033',
+      borderWidth: 1,
   },
-});
+  buttonStyle: {
+      flex: 1, justifyContent: "center", alignItems: "center", height: 50,
+      backgroundColor: "#eeeeee",
+      margin: 10
+  }
+  });
 
-export default Footer;
+  export default Footer;
