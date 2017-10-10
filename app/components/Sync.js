cad34ce3ab06ec45ccad5b056f7b5744eab7e61c
vishram.singh <vishram2997@gmail.com>
2017-10-10 01:17:00 -0700

    Added fetch from Navision ERP and save in realm

--- app/components/Sync.js
+++ app/components/Sync.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity,Alert } from 'react-native';
 import IconFA from 'react-native-vector-icons/FontAwesome';
+import Realm from './realm';
+import base64 from 'Base64';
 
 class Sync extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -9,11 +11,123 @@ class Sync extends Component {
     tabBarIcon: () => <IconFA name="cloud" size={22} />
   });
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        setting: Realm.objects('Setting'),
+    };
+    this._syncData = this._syncData.bind(this);
+  }
+  _syncData() {
+    let setting = Realm.objects('Setting');
+    console.info(JSON.stringify(setting));
+    let credential = "Test:123@Test";
+    let navUrl = "http://navserver.baqala.me:9348/Nav9Mob/OData/Company('Bodega Grocery Company LIVE')/";
+    if(setting.navUrl) {
+      navUrl = setting.navUrl;
+    }
+    if(setting.navUser) {
+      credential = setting.navUser +":"+setting.navPassword
+    }
+    console.log(`navUrlnavUrlnavUrlnavUrl ${navUrl}`);
+    console.log(`credentialcredentialcredential ${credential}`);
+    fetch(navUrl+'Vendors'+'?$format=json',
+      {
+      method: 'GET',
+      headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': "Basic " + base64.btoa(credential)
+    }}).then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        responseJson.value.map((dataItem)=>{
+            try {
+              Realm.write(() => {
+                  Realm.create('Vendor', {
+                    Id: dataItem.No,
+                    Name: dataItem.Name,
+                  },true);
+                });
+              }catch(e) {
+                console.log(e);
+              }
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  //Location
+  fetch(navUrl+'Stores'+'?$format=json',
+    {
+    method: 'GET',
+    headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': "Basic " + base64.btoa(credential)
+  }}).then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+      responseJson.value.map((dataItem)=>{
+          try {
+            Realm.write(() => {
+                Realm.create('Location', {
+                  Id: dataItem.POS_Department_ID,
+                  Name: dataItem.Location_Code,
+                  Password: dataItem.Password
+                },true);
+              });
+            }catch(e) {
+              console.log(e);
+            }
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+  //Item
+
+  fetch(navUrl+'Barcodes'+'?$format=json',
+    {
+    method: 'GET',
+    headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': "Basic " + base64.btoa(credential)
+  }}).then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson);
+      responseJson.value.map((dataItem)=>{
+          try {
+            Realm.write(() => {
+                Realm.create('Item', {
+                  barcode: dataItem.Bar_Code,
+                  No: dataItem.Item_No,
+                  description: dataItem.Description,
+                  unitCost: Number.parseFloat(dataItem.Unit_Cost),
+                  vendorId: dataItem.Vendor_No,
+                  vendorName:''
+                },true);
+              });
+            }catch(e) {
+              console.log(e);
+            }
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+
+}
   render() {
     return (
       <View style={styles.container}>
         <IconFA name="cloud" size={100} />
-        <TouchableOpacity style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.buttonContainer} onPress={
+          () => this._syncData()} >
           <Text style={styles.buttonText}>Tap to Sync</Text>
         </TouchableOpacity>
         <Text style={styles.syncText}>Last Sync: <Text>7</Text> hours ago</Text>
