1b1a2da997460f4a1faea3b4a6f1182801e49df5
user132944 <user132944@74-80-245-36.ic2net.net>
2017-11-18 00:13:56 -0800

    Integrated date component for IOS

--- app/components/reusable/DateField.js
+++ app/components/reusable/DateField.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import { DatePickerAndroid } from 'react-native';
+import { DatePickerAndroid, DatePickerIOS, Platform, Modal, View } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { formatDate, stringToDate } from '../../utils';
 import { setDate } from './DateField.actions';
-
+import Button from './Button';
 import Field from './Field';
 
 class DateField extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedDate: ''
+      selectedDate: '',
+      dateModalVisible: false
     };
 
     this._dateHandler = this._dateHandler.bind(this);
+    this._renderDatePickerIOS = this._renderDatePickerIOS.bind(this);
   }
 
   componentWillMount() {
@@ -23,20 +25,65 @@ class DateField extends Component {
   }
 
   _dateHandler() {
-    DatePickerAndroid.open({ date: stringToDate(this.state.selectedDate), minDate: new Date() })
-      .then(({ action, year, month, day }) => {
-        if (action === DatePickerAndroid.dateSetAction) {
-          this.setState({ selectedDate: formatDate(new Date(year, month, day)) });
-          this.props.setDate(formatDate(new Date(year, month, day)));
-        }
-      });
+    if (Platform.OS === 'android') {
+      DatePickerAndroid.open({ date: stringToDate(this.state.selectedDate), minDate: new Date() })
+        .then(({ action, year, month, day }) => {
+          if (action === DatePickerAndroid.dateSetAction) {
+            this.setState({ selectedDate: formatDate(new Date(year, month, day)) });
+            this.props.setDate(formatDate(new Date(year, month, day)));
+          }
+        });
+    }
+  }
+
+  _renderDatePickerIOS() {
+    return (
+      <View>
+        <Modal
+        animationType="slide"
+        transparent={false}
+        visible={this.state.dateModalVisible}
+        onRequestClose={() => this.setState({ dateModalVisible: false })}>
+        <View style={{ flex: 1 }}>
+          <View>
+            <DatePickerIOS date={stringToDate(this.state.selectedDate)} onDateChange={(date) => {
+                this.setState({
+                  selectedDate: formatDate(new Date(date)),
+                  dateModalVisible: false
+                });
+                this.props.setDate(formatDate(new Date(date)));
+              }} />
+          </View>
+
+          <View style={{ margin: 10 }}>
+            <Button text="Close" onPress={() => this.setState({ dateModalVisible: false })} />
+          </View>
+        </View>
+      </Modal>
+      </View>
+    );
   }
 
   render() {
+    // if (Platform.OS === 'android') {
     return (
-      <Field value={this.props.dateField.selectedDate} label="Date"
-        icon="calendar" editable={false} onPress={this._dateHandler} />
+      <View>
+        <Field value={this.props.dateField.selectedDate} label="Date"
+          icon="calendar" editable={false} onPress={() => {
+            if (Platform.OS === 'android') {
+              this._dateHandler();
+            } else {
+              this.setState({ dateModalVisible: true });
+            }
+          }} />
+        { this._renderDatePickerIOS() }
+      </View>
     );
+    // }
+
+    // return (
+    //   <Field value={this.props.dateField.selectedDate} label="Date"
+    // );
   }
 }
 
